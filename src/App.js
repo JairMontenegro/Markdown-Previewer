a89fb249357989c2190f6837c0e89a9d917ef9c1
Jair montenegro <jairmontenegrof@gmail.com>
2022-10-13 20:27:13 -0300

    add and repair npm mark

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.scss";
+import { marked } from "marked";
 import { useState } from "react";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(" # MARKDOWN PREVIEWER ");
 
   const input = (e) => {
     setText(e.target.value);
@@ -12,7 +13,12 @@ function App() {
   return (
     <div className="App">
       <textarea id="editor" onChange={input} value={text}></textarea>
-      <div id="preview">{text}</div>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(text),
+        }}
+      ></div>
     </div>
   );
 }
