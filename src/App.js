aaf452a6b87ba8cde14f1df63333b98f44040dd2
Jair montenegro <jairmontenegrof@gmail.com>
2022-10-14 20:07:42 -0300

    end project without styles and bonus

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import React from "react";
 import "./App.scss";
 import { marked } from "marked";
 import { useState } from "react";
+import Jair from "./img/Jair.svg";
 
 function App() {
-  const [text, setText] = useState(" # MARKDOWN PREVIEWER ");
+  const [text, setText] = useState(`# MARKDOWN PREVIEWER
+  ## Welcome to my markdown
+  [visit my portfolio](https://jairmontenegro.github.io/personal-portfolio-/)
+  
+  ---
+  \`npx-create-react-app .\`
+
+  \`\`\`let array = [10,20,40,-30,-20,50,-60];
+    let absMax = array.reduce(function(max, item){
+      return Math.max(Math.abs(max),Math.abs(item));
+      });
+  \`\`\`
+---
+1. Create project 
+2. write the funciton 
+3. Success
+---
+> Jair Montenegro Florez
+----
+**conquering  FreeCodeCamp front end libraries certification** 
+![jair dev](${Jair})
+
+
+
+`);
 
   const input = (e) => {
     setText(e.target.value);
